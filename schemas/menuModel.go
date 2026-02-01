6f77bb69e888a165a12890c96df716f55f010bac
mateusrws <mweliane2013@gmail.com>
2026-01-31 23:57:30 -0300

    refactor: update Menu model to use BaseModel and rename fields for consistency

--- schemas/menuModel.go
+++ schemas/menuModel.go
@@ -1,13 +1,11 @@
 package schemas
 
 import (
-	"gorm.io/gorm"
 	"github.com/google/uuid"
 )
 
 type Menu struct {
-	gorm.Model
-	id					 uuid.UUID
-	name         string
-	restaurantId uuid.UUID
+	BaseModel
+	Name         string
+	RestaurantId uuid.UUID
 }

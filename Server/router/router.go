ee28103fb7dda8756b0dc41a696e4ae119dc33f8
mateusrwx <mweliane2013@gmail.com>
2026-03-13 16:02:45 -0300

    Feat: Add the CORS configuration

--- Server/router/router.go
+++ Server/router/router.go
@@ -1,11 +1,19 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize(){
 	router := gin.Default()
+	
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
+	
 	initializeRoutes(router)
 	router.Run()
 }
